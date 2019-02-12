c77aadd4d6d406d9b8da4231fc9386453df13c75
Jaime Lopez <loama18@gmail.com>
2019-02-12 04:33:19 -0500

    add modal

--- src/components/charactersList.vue
+++ src/components/charactersList.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="charactersList">
     {{searchQuery}}
-    {{selectedCharacter}}
+
+    <div id="characterModal"
+         v-bind:class="{active: selectedCharacter !== ''}"
+         v-html="selectedCharacter.html_content">
+      {{selectedCharacter}}
+    </div>
+    <div id="modalOverlay"
+         v-on:click="selectedCharacter = ''"
+         v-bind:class="{active: selectedCharacter !== ''}">
+    </div>
+
     <ul>
       <li v-on:click="openModal(c.title)"
           v-for="c in characters"
@@ -9,6 +19,7 @@
         {{c.title}}
       </li>
     </ul>
+
   </div>
 </template>
 
@@ -27,7 +38,7 @@ export default {
   data () {
     return {
       characters: [],
-      selectedCharacter: null
+      selectedCharacter: ''
     }
   },
   methods: {
@@ -63,4 +74,44 @@ export default {
 <style lang="sass" scoped>
   ul
     list-style: none
+    max-height: calc(100vh - 80px)
+    overflow-y: scroll
+    padding-top: 16px
+
+    li
+      cursor: pointer
+
+  #characterModal
+    position: absolute
+    z-index: 11
+    top: -100vh
+    left: calc(10vw)
+    min-height: 320px
+    width: 80vw
+    max-width: 1000px
+    background: #FFFFFF
+    border: 1px solid #D0D0D0
+    transition: all 0.2s
+
+    &.active
+      transform: translate3d(0, calc(100vh - 80px), 0)
+
+  #modalOverlay
+    position: fixed
+    z-index: 10
+    top: 0
+    left: 0
+    height: 100vh
+    width: 100vw
+    background: #000000
+    opacity: 0.8
+    display: none
+
+    &.active
+      display: block
+
+
+  @media screen and (min-width: 1250px)
+    #characterModal
+      left: calc(50vw - 500px)
 </style>
