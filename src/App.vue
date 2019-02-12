53dbd188b6c811f0ce0e8e545a82545f7086b0a8
Jaime Lopez <loama18@gmail.com>
2019-02-12 02:00:35 -0500

    add sass, navbar and logo

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <navbar />
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
+import navbar from './components/navbar.vue'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
+    navbar,
     HelloWorld
   }
 }
@@ -23,6 +25,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 72px;
 }
 </style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="navbar">
+    <img alt="Naruto logo" src="../assets/logo.png">
+    <span class="title"> Characters </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass" scoped>
+  .navbar
+    position: fixed
+    top: 0
+    left: 0
+    z-index: 10
+    height: 56px
+    width: 100vw
+    border-bottom: 1px solid #E0E0E0
+
+    img
+      position: absolute
+      top: 4px
+      left: 16px
+      height: 48px
+
+    .title
+      position: absolute
+      top: 2px
+      left: 96px
+      line-height: 56px
+      font-size: 24px
+      font-weight: 600
+      letter-spacing: -1px
+      color: #A0A0A0
+</style>
